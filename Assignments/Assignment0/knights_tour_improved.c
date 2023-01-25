6efe07d77b3947a9596dda7921119685654c9f31
Rodrigo Santamaria <jrodsc@iastate.edu>
2023-01-24 23:23:43 -0600

    Adding feature: printing board

--- Assignments/Assignment0/knights_tour_improved.c
+++ Assignments/Assignment0/knights_tour_improved.c
@@ -11,6 +11,7 @@ bool cylr,cylc,boolt;
 int rs[8] = {-2,-2,-1,-1,1,1,2,2};
 int cs[8] = {-1,1,-2,2,-2,2,-1,1};
 int p;
+long long int total = 0;
 
 int* ans;
 
@@ -18,6 +19,7 @@ void tour(int r,int c){
 
     
     if(p >= R * C){
+        total++;
         fprintf(ftour,"%d", ans[0]);
         for(int j = 1; j < R * C; j++){
             fprintf(ftour,",%d", ans[j]);
@@ -81,12 +83,12 @@ int main(int argc, char *argv[]){
     printf("Please, insert the number of columns: ");
     scanf("%d", &C);
 
-    int t = -1;
+    long long int t = -1;
 
     do{
         printf("Is your board a cylinder? (1) Yes, (0) No ");
 
-        scanf("%d",&t);
+        scanf("%lld",&t);
     }
     while(t < 0 || t > 1);
 
@@ -95,7 +97,7 @@ int main(int argc, char *argv[]){
         do{
             printf("Is your board a torus? (1) Yes, (0) No ");
 
-            scanf("%d",&t);
+            scanf("%lld",&t);
         }
         while(t < 0 || t > 1);
 
@@ -107,7 +109,7 @@ int main(int argc, char *argv[]){
         do{
             printf("Which type of cylinder is your board? (1) Vertical, (0) Horizontal ");
 
-            scanf("%d",&t);
+            scanf("%lld",&t);
         }
         while(t < 0 || t > 1);
 
@@ -136,7 +138,70 @@ int main(int argc, char *argv[]){
 
     fclose(ftour);
 
-    
+    if(total == 0){
+        printf("There are no valid tours for this configuration\n");
+
+        return 0;
+    }
+
+    printf("The tours have been succesfully printed in tours.txt!\n");
+
+    do{
+        printf("\nDo you want to print any specific tour? (1) Yes (0) No" );
+        
+        scanf("%lld", &t);
+    }while(t < 0 || t > 1);
+
+    if(t == 1){
+        do{
+            printf("\n Choose a number between 1 and %lld: ",total);
+            scanf("%lld", &t);
+        }while(t < 1 || t > total);
+
+        ftour = fopen("tours.txt","r");
+        
+        char * st;
+        long unsigned int size = R * C - 1, tt = 1,cc = 1;
+
+        for(int x = 1; x < R * C + 1; x++){
+            if(x >= tt * 10){
+                tt *= 10;
+                cc++;
+            }
+            size += cc;
+        }
+
+        st = (char *) malloc(size);
+
+        for(long long int x = 0; x < t; x++){
+            getline(&st,&size, ftour);
+        }
+
+        printf("%s",st);
+        printf("\n");
+
+        int y = 0,c = 0;
+
+        for(long unsigned int x = 0; x < size && st[x] != '\n'; x++){
+            //printf("%d\n", y);
+            if(st[x] == ','){
+                ans[y - 1] = ++c;
+                y = 0;
+            }else{
+                y *= 10;
+                y += st[x] - '0';
+            }
+        }
+
+        ans[y - 1] = ++c;
+
+        for(i = 0; i < R; i++){
+            for(j = 0; j < C; j++){
+                printf("%3d", ans[i * R + j]);
+            }
+            printf("\n");
+        }
+    }
 
 
     return 0;
