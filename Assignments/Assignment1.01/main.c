af6147f80dc5045376e687f36dd4d0e8db534634
Rodrigo Santamaria <jrodsc@iastate.edu>
2023-01-29 17:05:44 -0600

    Path_generation added, looking good!

---
+++ Assignments/Assignment1.01/main.c
@@ -0,0 +1,176 @@
+//Net-ID: jrodsc
+//Rodrigo Santamaria
+//COM S 327 Assignment1.01
+
+// >= 2 TG :
+// >= 1 W ~
+// >= 2 SG
+// boulders or trees
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "queue.h"
+
+#define INF 1000000000
+#define ROWS 21
+#define COLUMNS 81 // real max 83
+
+//Basic functions
+
+int min(int  a, int  b){ 
+    if( a < b) return a; 
+    return b;
+}
+
+int max(int a, int b){ 
+    if( a > b) return a; 
+    return b;
+}
+
+//Global variables
+
+char terrain[ROWS][COLUMNS];
+int ancestor[ROWS][COLUMNS];
+int dist[ROWS][COLUMNS];
+int offset_r[4] = {0, 1, 0, -1};
+int offset_c[4] = {1, 0, -1 , 0};
+int west_exit, east_exit, north_exit, south_exit, cross_r, cross_c;
+
+//Process functions
+
+void search_ancestor(int r, int c){
+    int a;
+    while(!(r == cross_r && c == cross_c)){
+        printf("%d %d\n", r,c);
+        terrain[r][c] = '#';
+        a = ancestor[r][c];
+        r = a / COLUMNS;
+        c = a % COLUMNS;
+    }
+}
+        
+
+void paths_generation(){
+
+    int u,v;
+
+    for(u = 0; u < ROWS; u++){
+        for(v = 0; v < COLUMNS; v++){
+            ancestor[u][v] = -1;
+            dist[u][v] = INF;
+        }
+    }
+
+    dist[cross_r][cross_c] = 0;
+    ancestor[cross_r][cross_c] = -2;
+
+    queue_t q;
+
+    queue_init(&q);
+
+    queue_enqueue_front(&q, cross_r * COLUMNS + cross_c);
+
+    int tmp_r, tmp_c,r,c;
+
+    while(!queue_is_empty(&q)){
+        queue_dequeue(&q, &u);
+
+        r = u / COLUMNS;
+        c = u % COLUMNS;
+
+        printf("%d %d\n", r,c);
+
+        for(v = 0; v < 4 ; v++){
+            tmp_r = r + offset_r[v];
+            tmp_c = c + offset_c[v];
+
+            if(terrain[tmp_r][tmp_c] == '%') continue;
+            if(dist[r][c] + (int)(terrain[tmp_r][tmp_c] - '0') < dist[tmp_r][tmp_c]){
+                ancestor[tmp_r][tmp_c] = r * COLUMNS + c;
+                dist[tmp_r][tmp_c] = dist[r][c] + (int)(terrain[tmp_r][tmp_c] - '0');
+                if(terrain[tmp_r][tmp_c] == '#') continue;
+                if(terrain[tmp_r][tmp_c] == '0'){
+                    queue_enqueue_front(&q, tmp_r * COLUMNS + tmp_c);
+                }else{
+                    queue_enqueue_back(&q, tmp_r * COLUMNS + tmp_c);
+                }
+            }
+        }
+    }
+
+    for(u = 0; u < ROWS; u++){
+        for(v = 0; v < COLUMNS; v++){
+            printf("%d ", dist[u][v]);
+        }
+        printf("\n");
+    }
+
+    search_ancestor(west_exit, 0);
+    search_ancestor(east_exit, COLUMNS -2);
+    search_ancestor(0, north_exit);
+    search_ancestor(ROWS - 1, south_exit);
+
+    
+}
+
+
+
+void init(){
+    int i,j;
+
+    for(i = 0; i < ROWS; i++){
+        terrain[i][COLUMNS-1] = '\0';
+    }
+
+    for(i = 0; i < COLUMNS-1; i++){
+        terrain[0][i] = terrain[ROWS-1][i] = '%';
+    }
+
+    for(i = 0; i < ROWS; i++){
+        terrain[i][0] = terrain[i][COLUMNS-2] = '%';
+    }
+
+    west_exit = min(4 + rand()%ROWS, ROWS - 4);
+    east_exit = min(4 + rand()%ROWS, ROWS - 4);
+    north_exit = min(4 + rand()%COLUMNS, COLUMNS - 5);
+    south_exit = min(4 + rand()%COLUMNS, COLUMNS - 5);
+
+    for(i = 1; i < ROWS-1; i++){
+        for(j = 1; j < COLUMNS-2; j++){
+            terrain[i][j] = '0' + rand()%2;
+        }
+    }
+
+    terrain[west_exit][0] = terrain[east_exit][COLUMNS-2] = terrain[0][north_exit] = terrain[ROWS-1][south_exit] = '#';
+
+    cross_r = min(4 + rand()%ROWS, ROWS - 4);
+    cross_c = min(4 + rand()%COLUMNS, COLUMNS - 5);
+
+    terrain[cross_r][cross_c] = 'X';
+    
+    paths_generation();
+    
+    for(i = 1; i < ROWS-1; i++){
+        for(j = 1; j < COLUMNS-2; j++){
+            if(terrain[i][j] >= '0') terrain[i][j] = ':';
+        }
+    }
+}
+
+
+
+int main(int argc, char * argv[]){
+
+    srand(time(NULL));
+
+    init();
+
+    int i;
+  
+    for(i = 0; i < ROWS; i++){
+        printf("%s\n",terrain[i]);
+    }
+    return 0;
+}
