946b30f9554b1ee153eae9263dfc1a414e3069f7
Jrodsc <55962973+Jrodsc@users.noreply.github.com>
2023-09-15 12:04:30 -0500

    Delete Lectures directory

--- Lectures/Lecture 16/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "string327.h"
-#include <iostream>
-
-using namespace std;
-
-int main(int argc, char *argv[]){
-    return 0;
-}
--- Lectures/Lecture 17/main.c
+++
@@ -1,8 +0,0 @@
-#include "cpp-function.h"
-
-int main(int argc, char *argv[]){
-    ostream *o = get_cout();
-    use_cout(o, "Hello World!\n");
-
-    return 0;
-}
